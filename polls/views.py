87a817cd9f3347d631485ee49edea05d2d5ac108
Pengan Zhou <pengan1987@msn.com>
2018-10-18 11:19:59 -0700

    improve table display and page

--- polls/views.py
+++ polls/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    orders_list = Orders.objects.order_by('-trade_date')
+    orders_list = Orders.objects.order_by('trade_date')
     paginator = Paginator(orders_list, 25) # Show 25 contacts per page
 
     page = request.GET.get('page')
@@ -22,4 +22,12 @@ def index(request):
         # If page is out of range (e.g. 9999), deliver last page of results.
         orders = paginator.page(paginator.num_pages)
 
-    return render(request, 'polls/list.html', {'orders': orders})
+    index = orders.number - 1 
+    max_index = len(paginator.page_range)
+    
+    start_index = index - 4 if index >= 4 else 0
+    end_index = index + 5 if index <= max_index - 5 else max_index
+    
+    page_range = list(paginator.page_range)[start_index:end_index]
+
+    return render(request, 'polls/list.html', {'orders': orders, "page_range" : page_range})
